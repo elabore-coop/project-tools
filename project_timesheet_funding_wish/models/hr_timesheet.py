56f757505b73a526d50e38a01cb3a9b07eafe6a8
Stéphan Sainléger <54352366+stephansainleger@users.noreply.github.com>
2022-07-26 18:30:37 +0200

    [ADD] create ``project_timesheet_funding_wish`` add-on
    Add timesheet line fields:
    - ``funding_wish`` to inform about if the user expects a payment
    for the registered time.
    - ``treated`` to keep track if the timesheet line funding wish has
    been taken into account.
    
    Task: JOI-13
    Signed-off-by: Valentin Lab <valentin.lab@kalysto.org>

---
+++ project_timesheet_funding_wish/models/hr_timesheet.py
@@ -0,0 +1,16 @@
+from odoo import _, fields, models
+
+
+class AccountAnalyticLine(models.Model):
+    _inherit = "account.analytic.line"
+
+    funding_wish = fields.Selection(
+        [
+            ("free", "Free"),
+            ("accepted", "Payment accepted"),
+            ("expected", "Payment expected"),
+        ],
+        string=_("Funding wish"),
+        copy=False,
+    )
+    treated = fields.Boolean(string=_("Treated"), copy=False)
