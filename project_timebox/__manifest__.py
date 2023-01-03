8ee9f1d014a0b459afbc9c1666e0693c73c72678
Stéphan Sainléger <contact@stephansainleger.fr>
2023-01-03 12:25:44 +0100

    [MIG] project_timebox: migrate add-on to v13

--- project_timebox/__manifest__.py
+++ project_timebox/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "project_timebox",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "author": "Elabore",
     "website": "https://elabore.coop",
     "maintainer": "Stéphan Sainléger",
