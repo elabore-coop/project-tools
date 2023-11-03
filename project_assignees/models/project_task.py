9892abe9f89d9bd066af3790d0c3d195147dc797
Laetitia Da Costa <laetitia.dacosta@elabore.coop>
2023-11-03 12:28:21 +0100

    [FIX]project_assignees:add project manager as default assignee

--- project_assignees/models/project_task.py
+++ project_assignees/models/project_task.py
@@ -12,9 +12,12 @@ class Task(models.Model):
         '''
         assigned project manager to the task if nobody else is assigned to
         '''
-        assignee_ids = vals.get('assignee_ids', [])
-        project_id = vals.get('project_id', [])
-        if project_id and assignee_ids and not assignee_ids[0][2]:
+        assignee_ids = vals.get('assignee_ids')
+        project_id = vals.get('project_id')
+        if project_id and self.env['project.project'].browse(project_id):
             project = self.env['project.project'].browse(project_id)
-            if project and project.user_id: assignee_ids[0][2] = str(project.user_id.id)
+            if project.user_id:
+                default_assignee_id = project.user_id.id
+                if not assignee_ids or (assignee_ids and not assignee_ids[0][2]): #if assignee_ids doesnt existe or assignee_ids existe but is empty
+                    vals['assignee_ids'] = [[6,0,[default_assignee_id]]]
         return super().create(vals)
