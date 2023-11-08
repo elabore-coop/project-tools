16c1da3c42699833702c5cdac46cb85a04ff76b5
Boris Gallet <boris.gallet@gmail.com>
2023-11-08 15:03:06 +0100

    [MIG] project_timebox: Migration to 16.0

--- project_timebox/__manifest__.py
+++ project_timebox/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "project_timebox",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Elabore",
     "website": "https://elabore.coop",
     "maintainer": "Stéphan Sainléger",
