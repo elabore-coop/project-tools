204a6a764743b04cbb3945169419a5438db91747
Laetitia Da Costa <laetitia.dacosta@elabore.coop>
2025-04-16 15:59:29 +0200

    [IMP]project_working_time_task_portal:show only billage hours in portal timesheets

--- project_working_time_task_portal/__manifest__.py
+++ project_working_time_task_portal/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "project_working_time_task_portal",
-    "version": "16.0.1.0.1",
+    "version": "16.0.1.0.2",
     "author": "Elabore",
     "website": "https://elabore.coop",
     "maintainer": "Boris Gallet",
