7b983265fab29399cc45f603e42712561daab080
Boris Gallet <boris.gallet@gmail.com>
2025-07-08 10:20:45 +0200

    [ADD] project_usability_misc: Add "All Tasks" menu item

---
+++ project_usability_misc/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2025 Boris Gallet (Elabore)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "project_usability_misc",
+    "version": "16.0.1.0.0",
+    "author": "Elabore",
+    "website": "https://github.com/elabore-coop/project-tools",
+    "maintainer": "boris.gallet@elabore.coop",
+    "license": "AGPL-3",
+    "category": "Tools",
+    "summary": "Miscellaneous usability improvements for project management",
+    "depends": [
+        "base",
+        "project",
+    ],
+    "data": [
+        "views/project_task.xml",
+    ],
+    "demo": [],
+    "installable": True,
+    "auto_install": False,
+    "application": False,
+}
